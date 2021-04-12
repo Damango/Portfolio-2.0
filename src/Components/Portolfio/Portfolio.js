406264fda01eeae31ef9bd97c75452a05da06031
Justin Kessler <redcoderhd1@gmail.com>
2021-04-12 13:54:08 -0400

    Dynamic Background changes

--- src/Components/Portolfio/Portfolio.js
+++ src/Components/Portolfio/Portfolio.js
@@ -1,7 +1,6 @@
 import React, {useState, useRef, useEffect} from 'react';
 import ProjectSelector from "../ProjectSelector/ProjectSelector"
 
-import Canvas from "../Canvas/Canvas"
 
 import "./Portfolio.css"
 
@@ -19,21 +18,27 @@ const Portfolio = (props) => {
     return (<div className="portfolio-container">
         <div className="jumbotron-container">
      
-            <div className="name-header">JUSTIN KESSLER</div>
+            <div className="name-header"><span>JUSTIN KESSLER</span><div className="header-wipe"></div></div>
+           
+            <div className="sub-text-header">Welcome to my Development Portfolio</div>
+            <div className="sub-text">Check out my Github, Projects, Articles, and previous work that I've done. Feel free to contact me at any time</div>
             <div className="jumbotron-buttons-container">
                 <button className="jumbotron-button">View Projects</button>
                 <button className="jumbotron-button">My Github</button>
                 <button className="jumbotron-button">Contact Me</button>
             </div>
-            <div className="canvas-container">
-                <Canvas />
 
-    
+            <div className="spheres-container">
 
-                
+                    <span className="sphere" style={{top: Math.floor(Math.random() * 600), left:Math.floor(Math.random() * 500), animationName:'bounce1', animationDuration:(Math.random() * 3) + 's'}}> </span>
+                    <span className="sphere" style={{top: Math.floor(Math.random() * 600), left:Math.floor(Math.random() * 500),animationName:'bounce2', animationDuration:(Math.random() * 3) + 's'}}> </span>
+                    <span className="sphere"style={{top: Math.floor(Math.random() * 600), right:Math.floor(Math.random() * 500),animationName:'bounce3', animationDuration: (Math.random() * 3) + 's'}}> </span>
+                    <span className="sphere"style={{top: Math.floor(Math.random() * 600), right:Math.floor(Math.random() * 500),animationName:'bounce4', animationDuration: (Math.random() * 3) + 's'}}> </span>
             </div>
-       
+            
         </div>
+
+
         <div className="portfolio-projects-container">
       
             <ProjectSelector />
